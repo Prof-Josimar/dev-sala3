f8517eaaf70afb8a1552ba06e293cfcb4605498f
josimaribeiro <josimar.ribeiro@prof.eteprc.faetec.rj.gov.br>
2026-09-17 08:04:00 -0300

    first commit

--- subir.cmd
+++ subir.cmd
@@ -1,7 +1,7 @@
-echo "# dev-sala3 %date%  %time%" > README.md
+echo "# dev-sala3 %date% %time%" > README.md
 git init
 git add . -v
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Prof-Josimar/dev-sala3.git
+git remote set-url origin git@github.com:Prof-Josimar/dev-sala3.git
 git push -u origin main
